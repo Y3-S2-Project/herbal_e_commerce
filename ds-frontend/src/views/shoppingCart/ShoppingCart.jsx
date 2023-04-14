8103e2727f6da95931e23862e24b3c1099e9ae0a
IT20636524 <IT20636524@my.sliit.lk>
2023-04-14 23:48:27 +0530

    ShoppingCart

---
+++ ds-frontend/src/views/shoppingCart/ShoppingCart.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import CartItem from '../../components/cartItem/CartItem'
+import { TopNav } from '../../components'
+
+export default function ShoppingCart() {
+  return (
+    // <div>
+    //   <CartItem />
+    //   <CartItem />
+    //   <CartItem />
+    //   <CartItem />
+    // </div>
+    <div>
+      <TopNav />
+      <h1 className="tw-font-bold tw-text-lg tw-m-4">Shopping Cart</h1>
+      <div style={{ maxHeight: '450px', overflowY: 'auto' }}>
+        {/* Render cart items in a scrollable view */}
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <br />
+      <hr className="tw-border-t tw-border-gray-800 tw-dark:border-gray-300 tw-h-px tw-w-full tw-my-3" />
+      <div className="tw-justify-end tw-max-w-3xl tw-mx-auto tw-p-1 tw-flex-col tw-items-end ">
+        {/* Display total price */}
+        <p>Total Price: $25000</p>
+        <br />
+        <a
+          href="#"
+          className="tw-bg-blue-500 tw-text-white tw-py-2 tw-px-4 tw-rounded-lg tw-mt-1 tw-self-end tw-ml-auto tw-text-right"
+        >
+          Checkout
+        </a>
+      </div>
+    </div>
+  )
+}
