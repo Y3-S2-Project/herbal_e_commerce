e0085337d6302efaf3bafd50de0594e8f8c3204f
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-04-25 10:13:49 +0530

    seller review page

--- ds-frontend/src/views/reviews/sellerView/sellerReviewView.jsx
+++ ds-frontend/src/views/reviews/sellerView/sellerReviewView.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import { TopNav } from '../../../components'
 //this page contains the seller view of reviews where the seller can view
 //all the reviews that have beem made on them and their products
 
 export default function SellerReviewView() {
     return (
+        <>
+        <TopNav />
         <div>
-           <h1>Seller Reviews Page</h1> 
+            <h1>Seller Reviews</h1>
         </div>
+        </>
     )
 }
