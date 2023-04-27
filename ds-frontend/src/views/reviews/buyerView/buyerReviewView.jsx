46b21ecfcea5b910c1d64b9e2c87034c16613627
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2023-04-27 10:38:29 +0530

    buyerReviews updated

--- ds-frontend/src/views/reviews/buyerView/buyerReviewView.jsx
+++ ds-frontend/src/views/reviews/buyerView/buyerReviewView.jsx
@@ -4,22 +4,13 @@ import AddReviewCard from './addReview/addReview'
 import Grid from '@mui/material/Grid'
 
 export default function BuyerReviewView() {
-  const reviewCategory = {
-    reviewID: 'R018',
-    user: {
-      _id: '642d7b30adc38c896ac0a760',
-    },
-    product: {
-      _id: '643e9f9a6c3952d93b979657',
-    },
-  }
   return (
     <>
-      <Grid container spacing={2}>
+      {/* <Grid container spacing={2}>
         <Grid item xs={12}>
           <AddReviewCard reviewCategory={reviewCategory} />
         </Grid>
-      </Grid>
+      </Grid> */}
       <Reviews reviewCategory={{ user: '642d7b30adc38c896ac0a760' }} />
     </>
   )
